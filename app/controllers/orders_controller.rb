e9513056ed23fd5e6964f977414a9e3b21ef1849
parwej <planetckp@gmail.com>
2014-10-18 02:58:09 +0530

    Add order history pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,14 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
+  def sales
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
+  end
+
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
+  end
+
 
   # GET /orders
   # GET /orders.json
@@ -9,10 +17,7 @@ class OrdersController < ApplicationController
    @orders = Order.all
   end
 
-  # GET /orders/1
-  # GET /orders/1.json
-  def show
-  end
+  
 
   # GET /orders/new
   def new
@@ -21,9 +26,7 @@ class OrdersController < ApplicationController
     
   end
 
-  # GET /orders/1/edit
-  def edit
-  end
+  
 
   # POST /orders
   # POST /orders.json
@@ -47,29 +50,7 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /orders/1
- # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { head :no_content }
-     else
-        format.html { render action: 'edit' }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-   @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url }
-      format.json { head :no_content }
-    end
-  end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/views/orders/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@orders) do |order|
-  json.extract! order, :id, :address, :city, :state, :pincode
-  json.url order_url(order, format: :json)
-end
